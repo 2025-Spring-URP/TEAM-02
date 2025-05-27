0a374c1eefa96b667b17d8a0ed920387fffd8ac4
YongSeong <53935607+YongCastle@users.noreply.github.com>
2025-05-27 16:15:47 +0900

    Update filelist.f

--- design/filelist.f
+++ design/filelist.f
@@ -4,3 +4,15 @@ ${PCIE_HOME}/design/sverilog/PCIE_PKG.svh
 ${PCIE_HOME}/design/sverilog/AXI4_IF.sv
 ${PCIE_HOME}/design/sverilog/SAL_FIFO.sv
 ${PCIE_HOME}/design/sverilog/SAL_SDP_RAM.sv
+// User Adds
+${PCIE_HOME}/design/sverilog/DLL_TOP.sv
+
+${PCIE_HOME}/design/sverilog/DLL/DLL_Packetizer_v1.sv
+${PCIE_HOME}/design/sverilog/DLL/DLL_Retry_Monitor.sv
+${PCIE_HOME}/design/sverilog/DLL/DLL_Arbitor.sv
+${PCIE_HOME}/design/sverilog/DLL/DLL_WR.sv
+
+${PCIE_HOME}/design/sverilog/DLL/DLL_Decoder.sv
+${PCIE_HOME}/design/sverilog/DLL/DLL_DLLP_Generator.sv
+${PCIE_HOME}/design/sverilog/DLL/DLL_DLCMSM.sv
+${PCIE_HOME}/design/sverilog/DLL/DLL_RD.sv
